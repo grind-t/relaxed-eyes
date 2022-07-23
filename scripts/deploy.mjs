e61c78d3bd54388bcebd3b84e093d52df34024e2
ggalustov <ggalustov@ozon.ru>
2022-07-24 01:16:20 +0300

    fix deploy script

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,10 +1,10 @@
 import 'dotenv/config';
-import { env, argv } from 'process';
-import { $, echo } from 'zx';
+import { env } from 'process';
+import { $, argv, echo } from 'zx';
 
-const args = argv.slice(2);
+const flags = [argv.prod && '--prod'].filter(Boolean);
 
 $`vercel pull --token ${env.VERCEL_TOKEN}`
-	.then(() => $`vercel build --token ${env.VERCEL_TOKEN} ${args}`)
-	.then(() => $`vercel --scope ${env.VERCEL_SCOPE} --token=${env.VERCEL_TOKEN} --prebuilt ${args}`)
+	.then(() => $`vercel build --token ${env.VERCEL_TOKEN} ${flags}`)
+	.then(() => $`vercel --scope ${env.VERCEL_SCOPE} --token=${env.VERCEL_TOKEN} --prebuilt ${flags}`)
 	.then(({ stdout }) => echo(stdout));
